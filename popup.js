3cf08af26e9a24f43e1c5d084b5d6c474cfcd724
juan <19martj@gmail.com>
2021-04-23 14:58:28 -0400

    add todo to popup

--- popup.js
+++ popup.js
@@ -1,22 +1,33 @@
 // Initialize button with user's preferred color
 
+generate();
 document.getElementById("options").addEventListener("click", () =>{
     console.log("options_button clicked");
     chrome.tabs.create({'url': "/options.html"});
 });
 
-chrome.storage.onChanged = function () {
-    console.log("onload generating list");
-    chrome.storage.sync.get('urls', (result) =>{
-        let out = result['urls'];
+function generate() {
+    function appendList(out, typeList){
         console.log(out);
-        let linkList = document.getElementById("link-list");
+        let linkList = document.getElementById(typeList);
         out.forEach(function (item, index) {
-            console.log("adding link" + item);
+            console.log("adding item " + item);
             let newItem = document.createElement('dt');
             newItem.appendChild(document.createTextNode(item));
             linkList.appendChild(newItem);
         });
+    }
+    console.log("onload generating list");
+    chrome.storage.sync.get('urls', (result) =>{
+        let out = result['urls'];
+        appendList(out, 'link-list');
+
+    });
+
+    chrome.storage.sync.get('todoList', (result) =>{
+        let out = result['todoList'];
+        console.log(out);
+        appendList(out, "todo-list")
     });
 
 
