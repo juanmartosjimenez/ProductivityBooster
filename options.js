536b992d4185ee1abb7c53c592565f6257d8ae49
liamgknowles <knowles.l@northeastern.edu>
2021-04-23 13:18:42 -0400

    Basic addwebsite options.html functionality

--- options.js
+++ options.js
@@ -1,28 +1,57 @@
-document.getElementById("addButton").addEventListener("click", myFunction);
+document.getElementById("addButton").addEventListener("click", AddWebsite);
+document.getElementById("addTodo").addEventListener("click", AddTodo);
 
-const data = []; // creating array
+const webData = []; // creating array for Websites
+const todoData = {}; //creating dictionary for todoItems
 
-function myFunction(){
-    if (data.includes(document.getElementById('t1').value)) {
-        const index = data.indexOf(document.getElementById('t1').value);
+function AddWebsite(){
+    let curWebsite = document.getElementById('t1').value;
+    if (webData.includes(curWebsite)) {
+        let index = webData.indexOf(curWebsite);
         if (index > -1) {
-            data.splice(index, 1);
+            webData.splice(index, 1);
         }
     }
     else {
-        data.push(document.getElementById('t1').value); // adding element to array
+        webData.push(curWebsite); // adding element to array
     }
-    chrome.storage.sync.set({"urls": data});
+    chrome.storage.sync.set({"urls": webData});
     document.getElementById('t1').value = ''; // Making the text box blank
-    display(); // displaying the array elements
+    displayWebsites(); // displaying the array elements
 }
 
-function display() {
+function displayWebsites() {
     let str = '';
-    str = 'current elements in data array : ' + data.length + '<br>';
-    for (let i = 0; i < data.length; i++) {
-        str += i + ':' + data[i] + "<br >";  // adding each element with key number to variable
+    str = 'current elements in webData array : ' + webData.length + '<br>';
+    for (let i = 0; i < webData.length; i++) {
+        str += i + ':' + webData[i] + "<br >";  // adding each element with key number to variable
     }
 
     document.getElementById('display').innerHTML = str; // Display the elements of the array
 }
+
+function AddTodo(){
+    let todoItem = document.getElementById('itemTodo').value;
+    let todoTime = document.getElementById('timeTodo').value;
+    if (todoTime < .1 || todoItem == "") {
+        alert("Please enter valid time (greater that .1 hours) and valid todo item.")
+    }
+    else {
+        todoData[todoItem] = todoTime;
+    }
+    chrome.storage.sync.set({"todoList": todoData});
+    document.getElementById('timeTodo').value = ''; // Making the text box blank
+    document.getElementById('itemTodo').value = ''; // Making the text box blank
+    displayTodo(); // displaying the array elements
+}
+
+function displayTodo() {
+    let str = '';
+    str = 'current elements in todoData array : ' + '<br>';
+    for(let i in todoData) {
+        str += i + ": " + todoData[i] + "<br />";
+    }
+
+    document.getElementById('displayTodo').innerHTML = str; // Display the elements of the array
+}
+
